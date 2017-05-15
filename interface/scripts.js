340244cf5361ded2261bbe28ab844f362155c9cd
aidancrowther <aidan.crowther@outlook.com>
2017-05-14 23:42:54 -0400

    Settings page updates

--- interface/scripts.js
+++ interface/scripts.js
@@ -1,15 +1,26 @@
 var portList = {};
+var allIps = {};
+var config = {};
+var hosts = {};
 
 $(document).ready(function(){
 	getPorts();
+	writeConfig();
 	getList();
 	$('#updateBtn').click(function(){
-		$.get('/update')
-		.done(function(){
-			getPorts();
-			getList();
-		});
+		updateConfig();
+		$.post('/update', config)
+			.done(function() {
+                $.get('/update')
+                    .done(function(){
+                        getPorts();
+                        getList();
+                    });
+			});
 	});
+    $('#changePortBtn').click(function(){
+		console.log('update');
+    });
 	$('#loginButton').click(function(){
 
 	});
@@ -17,36 +28,113 @@ $(document).ready(function(){
 
 function getList(){
 	$.get('/hosts', function(hostList){
+		hosts = hostList;
 		$('#pages').empty();
-		console.log(hostList);
         for(var key in hostList){
         	var ports = [hostList[key]['port']];
         	if(typeof hostList[key]['port'] === 'string') ports = hostList[key]['port'].split(', ');
         	for(var port in ports) {
-                var div = $('<div id=' + key + '-' + ports[port] + ' class="host">' + hostList[key]['reverse'] + '</div>');
+                var div = $('<div id=' + key + '-' + ports[port] + ' class="host"><p>' + hostList[key]['reverse'] + '</p></div>');
                 if (hostList[key]['thumbnails']) addThumbnail(div, ports[port], hostList[key]['thumbnails']);
                 $(div).click(redirect);
                 $('#pages').append($(div));
             }
         }
 	})
+		.done(function(){
+			writeConfig();
+		});
 }
 
 function addThumbnail(element, port, thumbnails){
+	if(typeof port == 'string') if(port.includes('/')) port = port.split('/')[0];
 	for(var key in thumbnails){
 		if(portList[thumbnails[key].split('.')[0]] == port){
-			$(element).text($(element).text()+" - "+thumbnails[key].split('.')[0]);
+			$(element).html($(element).html()+'</p><p>'+thumbnails[key].split('.')[0]);
 			$(element).css('background-image', 'url(images/'+thumbnails[key]+')')
         }
 	}
 }
 
+function writeIps(id){
+	$('#'+id).empty();
+	for(var ip in allIps){
+		var omitted = '';
+		var forced  = '';
+		if(allIps[ip]['omit']) omitted = ' - Ignored';
+		if(allIps[ip]['forced']) forced = ' - Forced';
+		if(!allIps[ip]['reverse'] && !allIps[ip]['forced']) omitted = ' - Ignored';
+		$('#'+id).append('<option>'+allIps[ip]['reverse']+' - '+allIps[ip]['ip']+omitted+forced+'</option>');
+	}
+}
+
+function writeConfig(){
+	getIps();
+	$.get('/config', function(data){
+		config = data;
+	})
+		.done(function(){
+			$('#ipStart').val(config['ipScanStart']);
+			$('#ipEnd').val(config['ipScanEnd']);
+			var ports = config['ports'];
+			$('#ports').empty();
+			for(var port in ports){
+				var service = '';
+				for(var serviceName in portList) if(portList[serviceName] == ports[port]) service = " - "+serviceName;
+				$('#ports').append('<option>'+ports[port]+service+'</option>');
+			}
+			$('#domain').val(config['domain']);
+			var state = "False";
+			if(config['ignoreHost']) state = "True";
+			$('#ignoreHost').val(state);
+			$('#redirects').empty();
+			var redirects = config['redirect'];
+			for(var key in redirects){
+				$('#redirects').append('<option>'+key+':'+redirects[key][0]+' => '+key+':'+redirects[key][0]+redirects[key][1]+'</option>');
+			}
+		})
+}
+
+function updateConfig(){
+    config['ipScanStart'] = $('#ipStart').val();
+    config['ipScanEnd'] = $('#ipEnd').val();
+    config['domain'] = $('#domain').val();
+
+    var ignoreHost = false;
+    if($('#ignoreHost').val() == "True") ignoreHost = true;
+    config['ignoreHost'] = ignoreHost;
+
+    var omissions = $('#ipOmit').val();
+    for(var omission in omissions){
+    	var host = {};
+    	for(var ip in allIps){
+    		if(allIps[ip]['ip'] == omissions[omission].split(' - ')[1]) host = allIps[ip];
+		}
+		if(host['forced']) config['ipForce'].splice(config['ipForce'].indexOf(host['ip']), 1);
+		else if(!host['forced'] && !host['reverse']) config['ipForce'].push(host['ip']);
+		else if(host['omit']) config['ipOmit'].splice(config['ipOmit'].indexOf(host['ip']), 1);
+		else config['ipOmit'].push(host['ip']);
+	}
+
+	if(config['ipOmit'] == [] || !config['ipOmit']) config['ipOmit'] = [''];
+    if(config['ipForce'] == [] || !config['ipForce']) config['ipForce'] = [''];
+
+    console.log(config);
+}
+
 function redirect(){
-	
+	window.location = 'http://'+$(this).attr('id').split('-')[0]+':'+$(this).attr('id').split('-')[1];
 }
 
 function getPorts(){
     $.get('/ports', function(data){
         portList = data;
+    })
+}
+
+function getIps(){
+    $.get('/ips', function(data){
+        allIps = data;
+        writeIps('ipOmit');
     })
 }
--- server.js
+++ server.js
@@ -7,14 +7,18 @@ var express = require('express');
 var app = express();
 var evilscan = require('evilscan');
 var ip = require('ip');
+var bodyParser = require('body-parser');
 
 //program constants
 const ROOT = './interface';
 const PORT = 8080;
-//const IP = ip.address();
-const IP = "127.0.0.1";
+const IP = ip.address();
+
+//global variables
+var allIps = {};
 
 //static file server
+var urlEncodedParser = bodyParser.urlencoded({ extended: true });
 app.use(express.static('interface'));
 
 //respond to request for index.html
@@ -34,12 +38,14 @@ app.get('/update', function(req, res){
     var ipForce = config['ipForce'];
     var ignoreHost = config['ignoreHost'];
     var thumbnails = {};
+    var redirects = config['redirect'];
+    var omitHosts = config['omitHosts'];
 
     //scan for thumbnails
     var images = fs.readdirSync('./interface/images');
     for(var image in images){
         var current = images[image].split('.')[0];
-        if(thumbnails[current]) thumbnails[current].append(images[image]);
+        if(thumbnails[current]) thumbnails[current].push(images[image]);
         else thumbnails[current] = [images[image]];
     }
 
@@ -57,10 +63,16 @@ app.get('/update', function(req, res){
     var scanner = new evilscan(options);
 
     scanner.on('result', function(data){
+        allIps[data['ip']] = data;
+        allIps[data['ip']]['omit'] = false;
+        allIps[data['ip']]['forced'] = false;
+        if(data['reverse']) if(data['reverse'].includes('.')) allIps[data['ip']]['reverse'] = allIps[data['ip']]['reverse'].split('.')[0];
+        if(ipOmit.includes(data['ip']) || omitHosts.includes(data['reverse'])) allIps[data['ip']]['omit'] = true;
+        if(ipForce.includes(data['ip'])) allIps[data['ip']]['forced'] = true;
         //ignoreHost if set true
         if(ignoreHost && data['ip'] === IP);
         //ignore devices without a hostname, unless they have been whitelisted
-        else if(data.hasOwnProperty('reverse') || ipForce.indexOf(data['ip']) >= 0) if(ipOmit.indexOf(data['ip']) < 0){
+        else if(data.hasOwnProperty('reverse') || ipForce.indexOf(data['ip']) >= 0) if(omitHosts.indexOf(data['reverse']) < 0 && ipOmit.indexOf(data['ip']) < 0){
             //set the name field of whitelisted servers to their IP
             if(ipForce.indexOf(data['ip']) >= 0) data['reverse'] = data['ip'];
             //strip host domain from names
@@ -71,27 +83,46 @@ app.get('/update', function(req, res){
     scanner.on('error', function(err){ console.log(err); });
     scanner.on('done', function(){
         var devices = {};
-        for(var device in results){
+        for(var device in results) {
             var current = results[device];
             //write hosts to json object, removing duplicates
-            if(!devices[current['reverse']]){
+            if (!devices[current['reverse']]) {
                 devices[current['reverse']] = current;
+                devices[current['reverse']]['port'] = current['port'];
+                devices[current['reverse']]['thumbnails'] = [];
             }
-            else devices[current['reverse']]['port'] += ', '+current['port'];
-            for(var port in portList){
-                if(current['port'] === portList[port]){
-                    if(thumbnails.hasOwnProperty(port)) devices[current['reverse']]['thumbnails'] = thumbnails[port];
+            else devices[current['reverse']]['port'] += ', ' + current['port'];
+            for (var port in portList) {
+                if (current['port'] === (portList[port])) {
+                    if(thumbnails.hasOwnProperty(port)) devices[current['reverse']]['thumbnails'].push(thumbnails[port][0]);
+                    if(redirects.hasOwnProperty((current['reverse']))){
+                        if(redirects[current['reverse']][0] == portList[port]){
+                            var toRedirect = String(devices[current['reverse']]['port']);
+                            devices[current['reverse']]['port'] = toRedirect.replace(redirects[current['reverse']][0], redirects[current['reverse']][0]+redirects[current['reverse']][1]);
+                        }
+                    }
                 }
             }
         }
         //write hosts to hosts.json
         fs.writeFile('hosts.json', JSON.stringify(devices), function(err){
-            if(err) console.log(err);
-            //send completion notice to browser
-            else res.sendStatus(200);
+            if(err) res.sendStatus(500);
         });
+        //write ips to ips.json
+        fs.writeFile('ips.json', JSON.stringify(allIps), function(err){
+            if(err) res.sendStatus(500);
+        });
+        res.sendStatus(200);
     });
     scanner.run();
+});
+
+//update config settings
+app.post('/update', urlEncodedParser, function(req, res){
+    fs.writeFile('config.json', JSON.stringify(req.body), function(err){
+        if(err) res.sendStatus(500);
+        res.sendStatus(200);
+    });
 });
 
 //respond to request for host list
@@ -102,6 +133,16 @@ app.get('/hosts', function(req, res){
 //respond to requests for port types
 app.get('/ports', function(req, res){
     res.send(JSON.parse(fs.readFileSync('ports.json')));
+});
+
+//return a list of all ips within the range, regardless of config
+app.get('/ips', function(req, res){
+   res.send(JSON.parse(fs.readFileSync('ips.json')));
+});
+
+//return config settings
+app.get('/config', function(req, res){
+    res.send(JSON.parse(fs.readFileSync('config.json')));
 });
 
 //listen for requests on port 8080
